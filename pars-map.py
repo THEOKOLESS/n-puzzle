c36a3ddc56de33d1cd7db5c10ed6b785d547fc17
Theophile Vitoux <tvitoux@f0r5s12.codam.nl>
2021-09-28 19:52:33 +0200

    target pas bon

--- pars-map.py
+++ pars-map.py
@@ -5,15 +5,126 @@ import re
 def main():
     lines = []
     if len(sys.argv) == 2:
-        with open(sys.argv[1]) as f:
-            lines = f.readlines()
+        try:
+            with open(sys.argv[1]) as f:
+                lines = f.readlines()
+        except : 
+            print(f"impossible d'ouvrir {sys.argv[1]}")
+            exit()
         lines = clear_comments(lines)
-        check_map_validity(lines)
+        n = check_map_validity(lines)
+        #print(f"n -> {n}")
 
+        target = build_target(n) 
+        print(f"target offi -> {target}")
         #print(f"YYYEEEES {lines}")
         
     else:
         print("usage : incorrect")
+def build_target(n):
+    # i = 0
+    a = 0
+    # b = 1
+    # j = 2
+    target_index = 0
+    target_filler = 1
+    target = ['' for i in range(n * n)]
+
+    # while i < n:
+    # for i in range(n):
+    #     target[i] = target_filler
+    #     target_filler += 1
+        # i += 1
+    # for x in range(n - a):
+    #     target[n * j - b] = target_filler
+    #     j+=1
+    #     target_filler += 1
+    # b += 1 
+    # j -= 1
+    # for x in range(n - a):
+    #     test = (n * j) - x - b
+    #     print(f"test->{test}")
+    #     target[test] = target_filler
+    #     target_filler += 1
+    # a += 1
+    # b += n - a
+    # for x in range(n - a):
+    #     j -= 1
+    #     test = (n * j) - b 
+    #     print(f"test2->{test}")
+    #     target[test] = target_filler
+    #     target_filler += 1
+    # # a += 1
+    # for x in range(n - a):
+    #     target[test + x + 1] = target_filler
+    #     target_filler += 1
+    while target_filler <  len(target):
+        target_index = horizontal_up(target, target_filler, n, a, target_index)
+        target_filler += n - a
+        a += 1
+        
+ 
+
+        target_index = vertical_down(target, target_filler, n, a, target_index)
+        target_filler += n - a
+      
+        target_index = horizontal_down(target, target_filler, n, a, target_index)
+        target_filler += n - a
+     
+        a += 1
+        target_index = vertical_up(target, target_filler, n, a, target_index)
+        target_filler += n - a
+     
+        print(f"target -> {target} target_filler -> {target_filler} target_index -> {target_index}")
+        break
+
+        # break
+        # horizontal_down()
+        # vertical_up()
+        # horizontal_up()
+
+
+        # for x in range(n - a):
+        #     target_index = n * j - b
+        #     target[target_index] = target_filler
+        #     j+=1
+        #     target_filler += 1
+    return target
+def horizontal_up(target, target_filler, n, a, target_index):
+    for x in range(n - a):
+       
+        if target_filler == len(target):
+            target_filler = 0
+        target[target_index] = target_filler
+        target_index += 1
+        target_filler += 1
+    return target_index - 1 
+
+
+def horizontal_down(target, target_filler, n, a, target_index):
+    for x in range(n - a):
+        if target_filler == len(target):
+            target_filler = 0
+        # print(target_index)
+        target[target_index] = target_filler
+        target_index -= 1
+        target_filler += 1
+    return target_index + 1
+def vertical_down(target, target_filler, n, a, target_index):
+    # j = 1
+    for x in range(n - a):
+        target_index = target_index + n 
+        target[target_index] = target_filler
+
+        # j+=1
+        target_filler += 1
+    return target_index - 1
+def vertical_up(target, target_filler, n, a, target_index):
+    for x in range(n - a):
+        target_index = target_index - n 
+        target[target_index] = target_filler
+        target_filler += 1
+    return target_index
 
 def check_map_validity(lines):
     count = 0
@@ -39,32 +150,30 @@ def check_map_validity(lines):
                 count += 1
         else:
             print("invalid map")
-            exit() 
+            exit()
     if len(all_nbr) == len(set(all_nbr)) and len(all_nbr) == nxn: # pas de doublons et colones ok
         while j < nxn - 1:
             j+=1
-            check_validity_nbr +=   j
+            check_validity_nbr += j
         if check_validity_nbr != sum(all_nbr):
-            print("invalid map")
-            exit()
+            print("at least one number is akward")
+            exit()  
     else:
-        print("invalid map")
-        exit() 
+        print("col not right or same number")
+        sys.exit() 
     #         print (f"{check_validity_nbr}, {j}" )
 
-    print(all_nbr)
+    print(f"start -> {all_nbr}")
+    return n
     # print(sum(all_nbr))
 
 def clear_comments(lines):
     ret = []
     for line in lines:
         new_line = line.split("#")
-        if len(new_line) > 1:
+        if len(new_line) >= 1:
             if new_line[0] != '':
                 ret.append(new_line[0].strip()) 
-        elif len(new_line) == 1:
-            #print(f" LINE IN THE LOOP -> {new_line}")
-            ret.append(new_line[0].strip()) 
     return ret
 
 if __name__ == "__main__":
