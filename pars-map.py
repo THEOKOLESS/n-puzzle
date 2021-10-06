c99b3532328476dfdfa9a3d202f8c99bc8919fca
Theophile Vitoux <tvitoux@f0r3s7.codam.nl>
2021-10-06 17:38:19 +0200

    odd n solvability checked, stil need to check even

--- pars-map.py
+++ pars-map.py
@@ -23,9 +23,21 @@ def main():
 
 def is_it_solvable(target, lines):
     puzzle = make_clean_list(lines[1:])
+    
+    n = int(lines[0])
     blank = puzzle.index(0)
     blank_offi = target.index(0)
-    target_blank = abs(blank - blank_offi) % 2 
+    target_blank = abs(blank - blank_offi)
+    # if n % 2 == 1:
+    #     blank_offi = target.index(0)
+    #     target_blank = abs(blank - blank_offi)
+    # else:
+    #     blank = (blank + 1 ) /n
+    #     print(int(blank))
+    #     if int(blank) % 2 == 1:
+    #         target_blank = 0
+    #     else:
+    #         target_blank = 1
     inversion = 0
     print(f"target -> {target}")
     print(f"puzzle -> {puzzle}")
@@ -38,8 +50,17 @@ def is_it_solvable(target, lines):
         print(f"target_pos_list -> {target_pos_list}")
         print(f"pour le numero {pos}")
         print(f"puzzle_pos_list -> {puzzle_pos_list}")
-        print("---------------------------------------")
-        #comparer les 2 listes, envoyer si puzzle_pos_list, compter cb d'elem sont egalement dans target et add ca au nombre d'inversion ! 
+
+        for i in target_pos_list:
+            inversion += puzzle_pos_list.count(i)
+    print(f"Inversion nbr -> {inversion}")
+    print(f"target blank -> {target_blank}")
+    print("---------------------------------------")
+
+    if target_blank % 2 == inversion % 2:
+        print("solvable")
+    else:
+        print("unsolvable")
 
  
 
